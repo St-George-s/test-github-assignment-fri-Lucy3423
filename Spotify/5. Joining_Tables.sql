ad086f703870c46f94cfa0f80cc66095a7d06aee
Lucy3423 <f2648l@stge.org.uk>
2024-11-14 14:06:06 +0000

    Finsihed Worksheeets

--- Spotify/5. Joining_Tables.sql
+++ Spotify/5. Joining_Tables.sql
@@ -22,7 +22,7 @@ JOIN Tracks T ON A.artist_id = T.artist_id
 GROUP BY A.artist_name;
 
 
-find all track names for Lizzo by using an equi join between Tracks and Artists
+-- find all track names for Lizzo by using an equi join between Tracks and Artists
 SELECT T.track_name
 FROM Tracks T, Artists A
 JOIN Tracks on A.artist_id = T.artist_id
@@ -39,13 +39,9 @@ GROUP BY T.track_name;
 
 -- Count the number of tracks in each genre for 2017 by using an equi join
 -- among Tracks, Albums and Genres
-
 SELECT T.track_name, COUNT(G.genre_id) as 'Genre Count'
 FROM Albums A, Tracks T, Genres G
 JOIN Tracks on T.album_id = A.album_id
 JOIN Genres on T.genre_id = G.genre_id
 WHERE A.release_year < 2020
 GROUP BY T.track_name;
-
--- join the track ids
--- join the genre ids 

a011512f1f45dff65b90777f4b65342dd75a1cf7
Jack <J.Walker-35@sms.ed.ac.uk>
2024-12-03 09:20:22 +0000

    Mock DB Test

--- Database - Mock Test/main.sql
+++ Database - Mock Test/main.sql
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 SELECT Event.eventName AS 'Event Name', Shop.name AS 'Shop Name', Event.maxAttendees AS 'Max Attendees', Event.eventDate AS 'Event Date' 
 FROM Event
@@ -22,6 +23,9 @@ WHERE shopID = (
     JOIN OpeningTime ON Shop.shopID = OpeningTime.shopID
     WHERE Shop.name = 'Zara')
 AND OpeningTime.date = '2024-12-24';
+=======
+DESCRIBE ALL TABLES;
+>>>>>>> 6c99ea2 (Mock DB Test)
 =======
 DESCRIBE ALL TABLES;
 >>>>>>> 6c99ea2 (Mock DB Test)

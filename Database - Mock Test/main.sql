3f6bd2e839631945b68e30562604d6d9868324d5
Lucy3423 <f2648l@stge.org.uk>
2024-12-10 09:18:50 +0000

    mock test

--- Database - Mock Test/main.sql
+++ Database - Mock Test/main.sql
@@ -1,1 +1,23 @@
-DESCRIBE ALL TABLES;
+
+SELECT Event.eventName AS 'Event Name', Shop.name AS 'Shop Name', Event.maxAttendees AS 'Max Attendees', Event.eventDate AS 'Event Date' 
+FROM Event
+JOIN Shop on Event.shopID = Shop.shopID;
+
+SELECT Shop.name AS 'Shop Name', Event.eventName AS 'Event Name'
+FROM Shop
+JOIN Event on Shop.shopID = Event.shopID
+WHERE Event.eventDate = '2024-12-25';
+
+
+SELECT *
+FROM OpeningTime;
+
+
+UPDATE OpeningTime 
+SET closeTime = '19:00'
+WHERE shopID = (
+    SELECT Shop.name
+    FROM Shop
+    JOIN OpeningTime ON Shop.shopID = OpeningTime.shopID
+    WHERE Shop.name = 'Zara')
+AND OpeningTime.date = '2024-12-24';

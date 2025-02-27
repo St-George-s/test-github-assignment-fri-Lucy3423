51454ac44aba44da5e496478c91465b4900c9eec
Lucy3423 <f2648l@stge.org.uk>
2025-02-27 14:25:53 +0000

    Finished SQL section

--- Courswork 2025/coursework-2025/main.sql
+++ Courswork 2025/coursework-2025/main.sql
@@ -1,9 +1,43 @@
 DESCRIBE ALL TABLES;
 
-SELECT AVG(valuation), comicTitle
+-- 2c
+SELECT comicTitle, issue, publisherName, AVG(valuation) AS 'valuation'
 FROM Comic
-WHERE valuation => (
+JOIN Publisher ON Comic.publisherID = Publisher.publisherID
+WHERE valuation > (
     SELECT AVG(valuation)
     FROM Comic
 ) + 300
 GROUP BY comicID;
+
+
+-- 2d
+SELECT characterName, SUM(valuation) as "Total Valuation"
+FROM Comic
+JOIN ComicCharacter ON Comic.comicID = ComicCharacter.comicID
+JOIN CharacterInfo ON ComicCharacter.characterID = CharacterInfo.characterID
+WHERE characterName LIKE "%Duck%" AND mainCharacter = 1
+GROUP BY characterName
+ORDER BY SUM(valuation) DESC;
+
+
+-- 2e
+SELECT comicTitle, issue, publisherName, SUM(valuation * 2) AS [Double Price]
+FROM Comic, Publisher, Series, CharacterInfo, ComicCharacter
+WHERE Series.seriesName = "The OK Seven"
+AND characterName = "Starlordly"
+AND Comic.publisherID = Publisher.publisherID
+AND Comic.seriesID = Series.seriesID
+AND Comic.comicID = ComicCharacter.comicID
+AND CharacterInfo.characterID = ComicCharacter.characterID
+GROUP BY Series.seriesID;
+
+
+
+
+
+
+
+
+
+
